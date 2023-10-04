0c67f3ce4966ade61c161b01db1c30f3076946a7
Alexandru Apraloe <alexandru.apraloe@gmail.com>
2023-10-04 10:46:12 +0000

    #25 refactor s3 bucket into terrahouse_aws module

--- main.tf
+++ main.tf
@@ -1,20 +1,33 @@
+terraform {
+    #backend "remote" {
+  #  hostname = "app.terraform.io"
+  #  organization = "ExamPro"
 
-# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
-# resource "random_string" "bucket_name" {
-#   lower = true
-#   upper = false
-#   length   = 32
-#   special  = false
-# }
+  #  workspaces {
+  #    name = "terra-house-1"
+  #  }
+  #}
+#   cloud {
+#     organization = "CTMSolutions"
 
-# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
-resource "aws_s3_bucket" "website_bucket" {
-    # Bucket Naming Rules
-  #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
-  # bucket = random_string.bucket_name.result
-  bucket = var.bucket_name
+#     workspaces {
+#       name = "terra-house-1"
+#     }
+#   }
+  # required_providers {
+  #   # random = {
+  #   #   source = "hashicorp/random"
+  #   #   version = "3.5.1"
+  #   # }
+  #   aws = {
+  #     source = "hashicorp/aws"
+  #     version = "5.19.0"
+  #   }
+  # }
+}
 
-  tags = {
-    UserUuid = var.user_uuid
-  }
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.user_uuid
+  bucket_name = var.bucket_name
 }
